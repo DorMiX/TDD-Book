392f03e6f3aeff6da1a3da67eafbea37c4c5a69f
DorMiX <dormany.robert@gmail.com>
2018-03-18 15:48:11 +0100

    Make new model List and use it

--- lists/models.py
+++ lists/models.py
@@ -3,5 +3,14 @@ from django.db import models
 # Create your models here.
 
 
+class List(models.Model):
+    """List model."""
+
+    pass
+
+
 class Item(models.Model):
+    """Item model."""
+    
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default='')
