36dc720daad5c7e8680954f01be7462a3ed9c8b4
DorMiX <dormany.robert@gmail.com>
2018-03-19 17:44:50 +0100

    Use Bootsrtap and own css to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
-import unittest
 
 MAX_WAIT = 10
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """New visitor tests."""
 
     def setUp(self):
