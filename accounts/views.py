f9ea723a41b25f5e2beb5fffaafca0686a734ef0
DorMiX <dormany.robert@gmail.com>
2018-04-04 09:33:56 +0200

    ch17l005

--- accounts/views.py
+++ accounts/views.py
@@ -5,4 +5,11 @@ from django.core.mail import send_mail
 
 
 def send_login_email(request):
+    email = request.POST['email']
+    # send_mail(
+    #     'Your login link for Superlists',
+    #     'body text tbc',
+    #     'noreply@superlists',
+    #     [email],
+    # )
     return redirect('/')
