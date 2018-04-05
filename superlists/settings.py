7cacabf871ece6f853560e0d0a9be685f7cabcd1
DorMiX <dormany.robert@gmail.com>
2018-04-05 17:59:38 +0200

    ch17l049

--- superlists/settings.py
+++ superlists/settings.py
@@ -147,3 +147,9 @@ LOGGING = {
     },
     'root': {'level': 'INFO'},
 }
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'drfort2012@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
