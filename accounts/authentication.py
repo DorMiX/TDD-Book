ac25c4790f4415875e998aa58155971579bfb4c9
DorMiX <dormany.robert@gmail.com>
2018-04-05 10:34:37 +0200

    ch17l026

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,5 +1,9 @@
+from accounts.models import User, Token
+
+
 class PasswordlessAuthenticationBackend(object):
     """Passwordless Authentication Backend."""
     def authenticate(self, uid):
         """Authenticate."""
-        pass
+        token = Token.objects.get(uid=uid)
+        return User.objects.get(email=token.email)
