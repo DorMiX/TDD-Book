0fe4b426dac3ed2f192b7435b4fff7e6313187dc
DorMiX <dormany.robert@gmail.com>
2018-04-05 10:43:54 +0200

    ch17l028

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -3,11 +3,13 @@ from accounts.models import User, Token
 
 class PasswordlessAuthenticationBackend(object):
     """Passwordless Authentication Backend."""
-    
+
     def authenticate(self, uid):
         """Authenticate."""
         try:
             token = Token.objects.get(uid=uid)
             return User.objects.get(email=token.email)
+        except User.DoesNotExist:
+            return User.objects.create(email=token.email)
         except Token.DoesNotExist:
             return None
