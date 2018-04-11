32e94acfea8630c49d5ca9533aa9d02fbbb291fd
DorMiX <dormany.robert@gmail.com>
2018-04-11 11:19:01 +0200

    ch18l006

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -7,6 +7,19 @@ import time
 MAX_WAIT = 10
 
 
+def wait(fn):
+    def modified_fn():
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
     """Functional test set up."""
 
@@ -45,7 +58,7 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     @wait
     def wait_to_be_logged_in(self, email):
-        """Wait ti be logged in."""
+        """Wait to be logged in."""
         self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
