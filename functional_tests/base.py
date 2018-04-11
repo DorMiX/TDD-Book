a1adb86fd006c692b1d120e2aaf14557f71a3ce0
DorMiX <dormany.robert@gmail.com>
2018-04-11 11:21:49 +0200

    ch18l008

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -41,16 +41,10 @@ class FunctionalTest(StaticLiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
+    @wait
     def wait_for(self, fn):
         """Wait for."""
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
+        return fn()
 
     def get_item_input_box(self):
         """Get item input text."""
